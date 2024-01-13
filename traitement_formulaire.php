e7cf9cf48f3dddad1a1d076861304a57d8406232
frivar <mekki.d.tarek@hotmail.com>
2024-01-13 10:21:37 -0500

    new change

---
+++ traitement_formulaire.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $entreprise = $_POST["entreprise"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "mekki.d.tarek@hotmail.com";
+    $subject_mail = "Nouveau message du formulaire de contact";
+    $message_mail = "Nom et prénom: $name\n";
+    $message_mail .= "Email: $email\n";
+    $message_mail .= "Entreprise: $entreprise\n";
+    $message_mail .= "Sujet: $subject\n";
+    $message_mail .= "Message:\n$message";
+
+    // Vous pouvez ajouter des entêtes supplémentaires si nécessaire
+    $headers = "From: $email";
+
+    // Envoyer l'e-mail
+    mail($to, $subject_mail, $message_mail, $headers);
+
+    // Rediriger l'utilisateur vers une page de confirmation
+    header("Location: confirmation.html");
+    exit();
+}
+?>
